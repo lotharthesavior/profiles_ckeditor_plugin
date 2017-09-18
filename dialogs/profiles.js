4a7dea0cc066ee1e264b1042a2fce8f5c8417252
Savio Resende <savio@minisisinc.com>
2017-09-18 11:23:08 -0700

    Added extra condition based on options. The first is to add the profile to the current text, the second is to add the profile info to another element in the page based on the selector passed.

--- dialogs/profiles.js
+++ dialogs/profiles.js
@@ -1,22 +1,17 @@
 
 CKEDITOR.dialog.add( 'profilesDialog', function ( editor ) {
 
-    profiles_temp_editor = editor;
-
     let users_options = $('#ckeditor-profiles-users').html();
 
     let html = '<h1 style="font-size: 20px !important;">Select the user to attach to the text:</h1>' +
                 '<div>' +
                     '<select style="padding:4px;border:1px solid #000;border-radius:3px;" name="profiles-user">' + users_options + '</select>' +
-                    '<br/>' +
-                    '<button onclick="profilesDialogAddUser()">Add User</button>' +
                 '</div>';
 
     return {
-        title: 'Abbreviation Properties',
+        title: 'User to be Linked to the Comment',
         minWidth: 400,
         minHeight: 200,
-
         contents: [
             {
                 id: 'tab-basic',
@@ -28,6 +23,41 @@ CKEDITOR.dialog.add( 'profilesDialog', function ( editor ) {
                     }
                 ]
             }
-        ]
+        ],
+        onOk: function(){
+            var editor = this.getParentEditor(),
+                name = $('[name="profiles-user"]').find(':selected').html(),
+                current_value = $('[name="profiles-user"]').val();
+
+            var user_style = "" +
+                "padding: 5px;" +
+                "margin: 5px;" +
+                "border-radius:3px;" +
+                "background: blue;" +
+                "color: #fff;";
+
+            if( profiles_options.place == "default" ) {
+                var new_element = "" +
+                    "&nbsp;<a profileof='" + current_value + "' href='mailto:" + current_value + "' style='" + user_style + "'>" +
+                    name +
+                    "</a>&nbsp;";
+
+                editor.insertHtml(new_element);
+            } else {
+                var new_element = "" +
+                    "<a profileof='" + current_value + "' href='mailto:" + current_value + "' style='" + user_style + "'>" +
+                    name +
+                    "</a>";
+
+                // build hidden input to be sent
+                var hidden_input = "<input type='hidden' name='attached_profiles[]' profileof='" + current_value + "' value='" + current_value + "' />";
+
+                var remove_button = "<a profileof='" + current_value +
+                    "' onclick='remove_profile(\"" + current_value + "\")'" +
+                    "' style='padding:5px;font-weight: bold;cursor: pointer;'>X</a>";
+
+                $(profiles_options.place).append(new_element + remove_button + hidden_input);
+            }
+        }
     };
 });
--- plugin.js
+++ plugin.js
@@ -14,21 +14,27 @@
  * @todo check how to work around the support for addContentsCss for versions older than 4.4 of CKEditor.
  *  The code that is failing is "editor.addContentsCss( pluginDirectory + 'styles/profiles.css' );"
  *
- * To list the Users options on the dialog select, put a json of the Array in a hidden element with id
- * "ckeditor-profiles-users". This way the plugin will place them correctly in the text, marking the
- * metadata attached to each element in the list (each element must be an object with attributes named
- * like html elements names).
+ * How to:
+ *
+ * Step 1.
+ * To list the Users options on the dialog select, put a json of the array in a hidden element with id
+ * "ckeditor-profiles-users".
+ * This way the plugin will place them correctly in the text, marking the metadata attached to each element
+ * in the list (each element must be an object with attributes named like html elements names).
+ *
+ * Step 2.
+ * Specify the profile options. If it is default, the profile link will appear in the text editor, if anywhere else,
+ * it will appear in the specified place (ID).
+ * The name of the hidden input will be "attached_profiles[]".
  */
 
-var profiles_temp_editor = null;
+var profiles_options = {
+    place: "#attached_profiles"
+    // place: "default"
+};
 
-function profilesDialogAddUser(){
-
-    // console.log($('[name="profiles-user"]').val());
-
-    if( profiles_temp_editor != null )
-        profiles_temp_editor.insertHtml();
-    profiles_temp_editor = null;
+function remove_profile( current_value ){
+    $("[profileof=\"" + current_value + "\"]").remove();
 }
 
 CKEDITOR.plugins.add( 'profiles', {
